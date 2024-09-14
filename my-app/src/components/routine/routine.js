94b9a86d1598669910c23fb2b20948f5ec52fedc
Nirvan <nir1r7@users.noreply.github.com>
2024-09-14 17:51:42 -0400

    Created a drag and drop feature for the routine creation table

--- my-app/src/components/routine/routine.js
+++ my-app/src/components/routine/routine.js
@@ -1,11 +1,13 @@
 import "../../routine.css";
 import React, { useState } from 'react';
+import { DndProvider, useDrag, useDrop } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
 
 function Routine() {
     const [entries, setEntries] = useState([]);
 
     const [selectedPosition, setSelectedPosition] = useState('');
-    const [time, setTime] = useState(5);
+    const [time, setTime] = useState("5");
 
     const addEntry = () => {
         if (selectedPosition) {
@@ -23,56 +25,99 @@ function Routine() {
         }
     };
 
-    return (
-        <div className="main">
-            <h1>Routine Tracker</h1>
+    // Delete entry handler
+    const deleteEntry = (indexToRemove) => {
+        setEntries(entries.filter((_, index) => index !== indexToRemove));
+    };
 
-            <div className="table">
-                <table>
-                    <thead>
-                        <tr className="headers">
-                            <th>#</th>
-                            <th> Position </th>
-                            <th> Time </th>
-                            <th></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr className="add">
-                            <td></td>
-                            <td> 
-                                <Dropdown 
-                                    selectedPosition={selectedPosition} 
-                                    setSelectedPosition={setSelectedPosition} 
-                                /> 
-                            </td>
-                            <td> 
-                                <Slider 
-                                    time={time} 
-                                    setTime={setTime} 
-                                /> 
-                            </td>
-                            <td>
-                                <Button onClick={addEntry} />
-                            </td>
-                        </tr>
-                        {/* Render the entries */}
-                        {entries.map((entry, index) => (
-                            <tr key={index}>
-                                <td>{index+1}</td>
-                                <td>{entry.position}</td>
-                                <td>{entry.time} sec</td>
-                                <td></td>
+    // Drag and Drop Logic
+    const moveRow = (dragIndex, hoverIndex) => {
+        const dragEntry = entries[dragIndex];
+        const newEntries = [...entries];
+        newEntries.splice(dragIndex, 1);
+        newEntries.splice(hoverIndex, 0, dragEntry);
+        setEntries(newEntries);
+    };
+
+    return (
+        <DndProvider backend={HTML5Backend}>
+            <div className="main">
+                <h1>Routine Tracker</h1>
+
+                <div className="table">
+                    <table>
+                        <thead>
+                            <tr className="headers">
+                                <th className="row-counter">#</th>
+                                <th>Position</th>
+                                <th>Time</th>
+                                <th>Action</th>
                             </tr>
-                        ))}
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody>
+                            <tr className="add-row">
+                                <td className="row-counter"></td>
+                                <td>
+                                    <Dropdown 
+                                        selectedPosition={selectedPosition} 
+                                        setSelectedPosition={setSelectedPosition} 
+                                    />
+                                </td>
+                                <td>
+                                    <Slider 
+                                        time={time} 
+                                        setTime={setTime} 
+                                    />
+                                </td>
+                                <td>
+                                    <Button onClick={addEntry} />
+                                </td>
+                            </tr>
+                            {/* Render the entries */}
+                            {entries.map((entry, index) => (
+                                <DraggableRow
+                                    key={index}
+                                    index={index}
+                                    entry={entry}
+                                    moveRow={moveRow}
+                                    deleteEntry={() => deleteEntry(index)}
+                                />
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
             </div>
-        </div>
+        </DndProvider>
     );
 }
 
-export default Routine;
+function DraggableRow({ entry, index, moveRow, deleteEntry }) {
+    const [, ref] = useDrag({
+        type: 'ROW',
+        item: { index },
+    });
+
+    const [, drop] = useDrop({
+        accept: 'ROW',
+        hover: (item) => {
+            if (item.index !== index) {
+                moveRow(item.index, index);
+                item.index = index;
+            }
+        },
+    });
+
+    return (
+        <tr ref={(node) => ref(drop(node))}>
+            <td className="row-counter">{index + 1}</td>
+            <td>{entry.position}</td>
+            <td>{entry.time} sec</td>
+            <td>
+                <button className='del-button' onClick={deleteEntry}>-</button>
+            </td>
+        </tr>
+    );
+}
 
 function Slider({ time, setTime }) {
     const handleChange = (event) => {
@@ -119,3 +164,5 @@ function Button({ onClick }) {
         </button>
     );
 }
+
+export default Routine;
