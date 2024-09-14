22f9d9ede2316543692233282ded8339ea5d904e
Sean-Ye-Student <sean171171school@gmail.com>
2024-09-14 15:12:21 -0400

    Added Clerk Provider

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,17 +6,42 @@ import Home from "./components/home/home.js"
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 import { ConvexProvider, ConvexReactClient } from "convex/react";
+import { ConvexProvider, ConvexReactClient, Authenticated, Unauthenticated, useQuery, useMutation } from "convex/react";
+import { ClerkProvider, useAuth, SignIn, UserButton } from "@clerk/clerk-react";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+
+import { api } from "./convex/_generated/api.js";
 
 const convex = new ConvexReactClient("https://greedy-warbler-756.convex.cloud");
 
+function Temp(){
+  const updateUser = useMutation(api.users.updateUser, {});
+  updateUser(); //Just going to update the user all the time
+}
+
 function App() {
+  
+
   return (
     <div className="App">
       <ConvexProvider client={convex}>
+      <ClerkProvider publishableKey="pk_test_YWN0aXZlLXJvdWdoeS04MS5jbGVyay5hY2NvdW50cy5kZXYk">
+      <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
       <Router>
         <div>
+<<<<<<< Updated upstream
           <Link to="/routine">Landing page =\ Routine</Link> <br />
           <Link to="/canvas">Routine =\ Yoga</Link>
+=======
+        <Unauthenticated>
+          <SignIn></SignIn>
+        </Unauthenticated>
+        <Authenticated>
+          <UserButton></UserButton>
+          <Temp></Temp>
+        </Authenticated>
+        <Link to="/routine">Go to routine</Link>
+>>>>>>> Stashed changes
           <Routes>
             <Route path="/" element = {<Home />} />
             <Route path="/routine" element={<Routine />} />
@@ -25,6 +50,8 @@ function App() {
         </div>
       </Router>
       </ConvexProvider>
+      </ConvexProviderWithClerk>
+      </ClerkProvider>
     </div>
   );
 }
