bbe319ebadb513ff8accd90606b2ebb8972dbca4
Nirvan <nir1r7@users.noreply.github.com>
2024-09-14 12:13:29 -0400

    Created Canvas component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,102 +1,13 @@
 import "./App.css";
-import * as pose from '@mediapipe/pose'
-import smoothLandmarks from 'mediapipe-pose-smooth'; // ES6
-import * as cam from "@mediapipe/camera_utils"
-import * as drawingUtils from "@mediapipe/drawing_utils"
 import {useRef, useEffect, useState} from "react"
 
-import SidePanel from "./components/home/home.js"
+import SidePanel from "./components/home/home.js";
+import Canvas from "./components/canvas/canvas.js";
 
 function App() {
-  const webcamRef = useRef(null)
-  const canvasRef = useRef(null)
-  var camera = null
-  const [didLoad, setdidLoad] = useState(false)
-
-  const [landmarks, setLandmarks] = useState([]);
-
-  function onResults(results){
-    const canvasElement = canvasRef.current
-    const canvasCtx = canvasElement.getContext("2d")
-
-    canvasCtx.save();
-    canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
-    canvasCtx.drawImage(results.image, 0, 0, canvasElement.width, canvasElement.height);
-
-    if (results.poseLandmarks) {
-      drawingUtils.drawConnectors(canvasCtx, results.poseLandmarks, pose.POSE_CONNECTIONS, { visibilityMin: 0.65, color: 'white' });
-      drawingUtils.drawLandmarks(canvasCtx, Object.values(pose.POSE_LANDMARKS_LEFT)
-          .map(index => results.poseLandmarks[index]), { visibilityMin: 0.65, color: 'white', fillColor: 'blue' });
-
-      drawingUtils.drawLandmarks(canvasCtx, Object.values(pose.POSE_LANDMARKS_RIGHT)
-          .map(index => results.poseLandmarks[index]), { visibilityMin: 0.65, color: 'white', fillColor: 'red' });
-
-
-      drawingUtils.drawLandmarks(canvasCtx, Object.values(pose.POSE_LANDMARKS_NEUTRAL)
-          .map(index => results.poseLandmarks[index]), { visibilityMin: 0.65, color: 'white', fillColor: 'white' });
-
-    console.log(results.poseLandmarks);
-    console.log(JSON.stringify(results.poseLandmarks));
-
-    setLandmarks(results.poseLandmarks);
-
-   }
-    canvasCtx.restore();
-  }
-
-  useEffect(() => {
-    fetch("http://localhost:5000/setVectors/" + JSON.stringify(landmarks)).then((response) => response.json).then(j => {
-      console.log(j);
-    })
-  }, [landmarks]);
-
-  useEffect(() => {
-    if(!didLoad){
-      const mpPose = new pose.Pose({
-        locateFile: (file) => {
-            return `https://cdn.jsdelivr.net/npm/@mediapipe/pose/${file}`;
-        },
-      });
-      mpPose.setOptions({
-        selfieMode: true,
-        modelComplexity: 1,
-        smoothLandmarks: true,
-        enableSegmentation: false,
-        smoothSegmentation: true,
-        minDetectionConfidence: 0.5,
-        minTrackingConfidence: 0.5,
-      });
-
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-      camera = new cam.Camera(webcamRef.current, {
-        onFrame:async() => {
-          const canvasElement = canvasRef.current
-          const aspect = window.innerHeight / window.innerWidth;
-          let width, height;
-          if (window.innerWidth > window.innerHeight) {
-              height = window.innerHeight;
-              width = height / aspect;
-          }
-          else {
-              width = window.innerWidth;
-              height = width * aspect;
-          }
-          canvasElement.width = width;
-          canvasElement.height = height;
-          await mpPose.send({image: webcamRef.current});
-        }
-      })
-      camera.start();
-
-      mpPose.onResults((results) => smoothLandmarks(results, onResults));
-      setdidLoad(true)
-    }
-  },[didLoad])
-
   return <div className="App">
     <div className="container">
-      <video className="input_video" ref={webcamRef}/>
-      <canvas ref={canvasRef} className='output_canvas' ></canvas>
+      <Canvas />
       <SidePanel/>
     </div>
   </div>;
