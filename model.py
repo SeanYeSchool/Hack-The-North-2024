b2a1f454f2a8007f4da600d4255190814de6cd06
utcjcao <chrisj.cao@mail.utoronto.ca>
2024-09-14 03:11:44 -0400

    wrote dataset class, train methods, model class

---
+++ model.py
@@ -0,0 +1,21 @@
+import warnings
+import torch.nn as nn
+import torch.nn.functional as F
+import torch_geometric.nn as gnn
+
+warnings.filterwarnings('ignore')
+
+class pose_rec_model(nn.Module):
+    def __init__(self, in_features, hidden_features, out_features, num_classes):
+        super(pose_rec_model, self).__init__()
+        self.gat1 = gnn.GATConv(in_features, hidden_features, heads = 4, concat = True, dropout = 0.2)
+        self.gat2 = gnn.GATConv(4 * hidden_features, out_features, heads = 2, concat = False)
+        self.classifier = nn.Linear(out_features, num_classes)
+
+    def forward(self, x, edge_index, batch):
+        x = self.gat1(x, edge_index)
+        x = F.relu(x)
+        x = self.gat2(x, edge_index)
+        x = gnn.global_max_pool(x, batch)
+        x = self.classifier(x)
+        return x 
