9d4438cb6dd6d8914097a98d637b6b2f71ced959
Steven Lin <yuchenglin0705@gmail.com>
2024-09-14 18:48:51 -0400

    Formatted the yoga screen

--- my-app/src/components/yoga/yoga.js
+++ my-app/src/components/yoga/yoga.js
@@ -7,7 +7,7 @@ import "../../App.css";
 
 const SidePanel = () => {
     return (
-    <div className="container">   
+    <div className="container-fluid">   
     <Button> 
         Good Morning
     </Button>
@@ -20,13 +20,30 @@ const SidePanel = () => {
 
 const Yoga = () => {
     return (
+    <><div>
+            <h2>Hello world!</h2>
+            <p>
+                This is a simple hero unit, a simple Hero-style component for calling extra
+                attention to featured content or information.
+            </p>
 
-    <div className="container">
-        <div className="frame">
-            <Canvas/>
+            <hr class="my-4" />
+
+            <p>
+                It uses utility classes for typography and spacing to space content out within the
+                larger container.
+            </p>
+            <button type="button" data-mdb-button-init data-mdb-ripple-init class="btn btn-primary">
+                Learn more
+            </button>
         </div>
-        <SidePanel/>
-    </div>
+        <div className="container-fluid">
+                <div className="frame">
+                    <Canvas />
+                </div>
+                <SidePanel />
+        </div>
+    </>
     );
 }
 
