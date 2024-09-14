300ebd1f0c04d5c71b82bfc41cc8574e77d71cea
Nirvan <nir1r7@users.noreply.github.com>
2024-09-14 17:01:56 -0400

    Fixed Module not found error with Convex

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,7 +5,6 @@ import Routine from "./components/routine/routine.js";
 import Home from "./components/home/home.js"
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
-import { ConvexProvider, ConvexReactClient } from "convex/react";
 import { ConvexProvider, ConvexReactClient, Authenticated, Unauthenticated, useQuery, useMutation } from "convex/react";
 import { ClerkProvider, useAuth, SignIn, UserButton } from "@clerk/clerk-react";
 import { ConvexProviderWithClerk } from "convex/react-clerk";
@@ -20,38 +19,33 @@ function Temp(){
 }
 
 function App() {
-  
-
   return (
     <div className="App">
       <ConvexProvider client={convex}>
-      <ClerkProvider publishableKey="pk_test_YWN0aXZlLXJvdWdoeS04MS5jbGVyay5hY2NvdW50cy5kZXYk">
-      <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
-      <Router>
-        <div>
-<<<<<<< Updated upstream
-          <Link to="/routine">Landing page =\ Routine</Link> <br />
-          <Link to="/canvas">Routine =\ Yoga</Link>
-=======
-        <Unauthenticated>
-          <SignIn></SignIn>
-        </Unauthenticated>
-        <Authenticated>
-          <UserButton></UserButton>
-          <Temp></Temp>
-        </Authenticated>
-        <Link to="/routine">Go to routine</Link>
->>>>>>> Stashed changes
-          <Routes>
-            <Route path="/" element = {<Home />} />
-            <Route path="/routine" element={<Routine />} />
-            <Route path="/canvas" element={<Yoga />} /> {/* Change the name of the Home component to Canvas */}
-          </Routes>
-        </div>
-      </Router>
+        <ClerkProvider publishableKey="pk_test_YWN0aXZlLXJvdWdoeS04MS5jbGVyay5hY2NvdW50cy5kZXYk">
+          <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+          <Router>
+            <div>
+              <Link to="/routine">Landing page =\ Routine</Link> <br />
+              <Link to="/canvas">Routine =\ Yoga</Link>
+            <Unauthenticated>
+              <SignIn></SignIn>
+            </Unauthenticated>
+            <Authenticated>
+              <UserButton></UserButton>
+              <Temp></Temp>
+            </Authenticated>
+            <Link to="/routine">Go to routine</Link>
+              <Routes>
+                <Route path="/" element = {<Home />} />
+                <Route path="/routine" element={<Routine />} />
+                <Route path="/canvas" element={<Yoga />} /> {/* Change the name of the Home component to Canvas */}
+              </Routes>
+            </div>
+          </Router>
+          </ConvexProviderWithClerk>
+        </ClerkProvider>
       </ConvexProvider>
-      </ConvexProviderWithClerk>
-      </ClerkProvider>
     </div>
   );
 }
