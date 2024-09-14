f1507c0ffff00beee6fc3203ab02e2d04a1cca69
Sean-Ye-Student <sean171171school@gmail.com>
2024-09-14 02:29:34 -0400

    Added Vectors API for backend communication

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,22 @@
 from flask import Flask
+import json
 app = Flask(__name__)
 
-@app.route("/sendVector/<int:x>/<int:y>")
-def getVector(x, y):
-    with open("vectors.txt", "a") as f:
-        f.write(f"{x} {y}\n")
-    return f"Post {x} {y}"
+@app.route("/setVectors/<string:vectors_json>")
+def getVector(vectors_json):
+    '''
+    [
+        [x, y, z, v] list of list of floats
+    ]
+    '''
+
+    vectors_list = json.loads(vectors_json)
+    with open("vectors.txt", "w") as f:
+        accumulator = ""
+        for vector in vectors_list:
+            accumulator += f"{vector[0]} {vector[1]} {vector[2]} {vector[3]}\n"
+
+        accumulator = accumulator.strip("\n")
+        f.write(accumulator)
+        
+    return f"Post {accumulator}"
