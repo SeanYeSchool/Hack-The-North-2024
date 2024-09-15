6a2720956d1554a5dda401ec270a374ed223ad7c
Steven Lin <yuchenglin0705@gmail.com>
2024-09-14 20:55:50 -0400

    Added voiceflow overlay on index.html

--- my-app/src/components/yoga/yoga.js
+++ my-app/src/components/yoga/yoga.js
@@ -20,28 +20,13 @@ const SidePanel = () => {
 
 const Yoga = () => {
     return (
-    <><div>
-            <h2>Hello world!</h2>
-            <p>
-                This is a simple hero unit, a simple Hero-style component for calling extra
-                attention to featured content or information.
-            </p>
-
-            <hr class="my-4" />
-
-            <p>
-                It uses utility classes for typography and spacing to space content out within the
-                larger container.
-            </p>
-            <button type="button" data-mdb-button-init data-mdb-ripple-init class="btn btn-primary">
-                Learn more
-            </button>
-        </div>
+    <>
         <div className="container-fluid">
                 <div className="frame">
                     <Canvas />
                 </div>
                 <SidePanel />
+                
         </div>
     </>
     );
