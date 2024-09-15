af5f9742942597bca30328f5ab69fca5798cbe98
Steven Lin <yuchenglin0705@gmail.com>
2024-09-14 23:32:16 -0400

    Changed so the navigation bar doesn't show up when on canvas

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,13 +2,13 @@ import "./App.css";
 
 import Yoga from "./components/yoga/yoga.js";
 import Routine from "./components/routine/routine.js";
-import Home from "./components/home/home.js"
+import Home from "./components/home/home.js";
 
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link, useLocation } from "react-router-dom";
 import { useState } from "react";
 
 import { ConvexProvider, ConvexReactClient } from "convex/react";
-import { ClerkProvider, useAuth} from "@clerk/clerk-react";
+import { ClerkProvider, useAuth } from "@clerk/clerk-react";
 import { ConvexProviderWithClerk } from "convex/react-clerk";
 
 const convex = new ConvexReactClient("https://greedy-warbler-756.convex.cloud");
@@ -21,22 +21,35 @@ function App() {
       <ConvexProvider client={convex}>
         <ClerkProvider publishableKey="pk_test_YWN0aXZlLXJvdWdoeS04MS5jbGVyay5hY2NvdW50cy5kZXYk">
           <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
-          <Router>
-            <div>
-              <Link to="/"> To Home</Link> <br/ >
-              <Link to="/routine">To Routine</Link> <br />
-              <Link to="/canvas">To Yoga</Link>
+            <Router>
+              <Navigation />
               <Routes>
-                <Route path="/" element = {<Home />} />
+                <Route path="/" element={<Home />} />
                 <Route path="/routine" element={<Routine entries={entries} setEntries={setEntries} />} />
-                <Route path="/canvas" element={<Yoga entries={entries}/>} />
+                <Route path="/canvas" element={<Yoga entries={entries} />} />
               </Routes>
-            </div>
-          </Router>
+            </Router>
           </ConvexProviderWithClerk>
         </ClerkProvider>
       </ConvexProvider>
     </div>
+  );
+}
+
+function Navigation() {
+  const location = useLocation();
+  const showLinks = location.pathname === '/' || location.pathname === '/routine';
+
+  return (
+    <>
+      {showLinks && (
+        <div>
+          <Link to="/">To Home</Link> <br />
+          <Link to="/routine">To Routine</Link> <br />
+          <Link to="/canvas">To Yoga</Link>
+        </div>
+      )}
+    </>
   );
 }
 
