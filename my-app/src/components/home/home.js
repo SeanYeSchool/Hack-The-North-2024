8154feb484c476111450b6dccff949f94ea942fb
Steven Lin <yuchenglin0705@gmail.com>
2024-09-14 22:16:02 -0400

    Moved components from Sean branch to Test-Clone branch

--- my-app/src/components/home/home.js
+++ my-app/src/components/home/home.js
@@ -1,12 +1,20 @@
-import { Authenticated, Unauthenticated, useMutation } from "convex/react";
-import { SignIn, UserButton } from "@clerk/clerk-react";
+import {Authenticated, Unauthenticated, useMutation} from "convex/react";
+import {SignIn, UserButton } from "@clerk/clerk-react";
 
+//Home imports these since api cannot be imported easily from home
+//import { api } from "./convex/_generated/api.js";
 import { api } from "../../convex/_generated/api.js";
-
-function Temp(){
+import Stack from "react-bootstrap/Stack";
+import Container from "react-bootstrap/Container"
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Image from "react-bootstrap/Image";
+import image from "./yoga-person.png";
+export function Temp(){
   const updateUser = useMutation(api.users.updateUser, {});
   updateUser(); //Just going to update the user all the time
 }
+
 
 function Home(){
 
@@ -14,13 +22,27 @@ function Home(){
         <div>
             Home page
             <Unauthenticated>
-              <SignIn></SignIn>
+              <Container>
+              <Row>
+                <Stack direction="horizontal">
+                <Col>
+                <img src={image} rounded />
+                </Col>
+                <Col>
+                <SignIn></SignIn>
+                </Col>
+                </Stack>
+              </Row>
+              </Container>
             </Unauthenticated>
             <Authenticated>
               <UserButton></UserButton>
               <Temp></Temp>
             </Authenticated>
+            
         </div>
+
+        
     )
 }
 
